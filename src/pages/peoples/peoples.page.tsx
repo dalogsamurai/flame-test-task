ec0515967dcb424ea8f31fc2ace831acd168bcb5
dalogsamurai <thelogsamurai@gmail.com>
2023-10-20 13:03:27 +0300

    feat(PeoplesPage): loading add

--- src/pages/peoples/peoples.page.tsx
+++ src/pages/peoples/peoples.page.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./peoples.page.sass";
+import PeopleItem from "../../components/people-item/people-item.component";
 
 const PeoplesPage = () => {
   const [peoples, setPeoples] = useState([]);
@@ -12,13 +13,23 @@ const PeoplesPage = () => {
 
     // console.log(resPeoples);
     setPeoples(resPeoples);
+    if (peoples) {
+      setLoading(false);
+    }
+    console.log(peoples);
   };
 
   useEffect(() => {
     getPeoples();
   }, []);
 
-  return <div className="peoples-page"></div>;
+  return (
+    <div className="peoples-page">
+      {isLoading && <div className="">Loading...</div>}
+
+      {!isLoading && peoples.map((item) => <PeopleItem item={item} />)}
+    </div>
+  );
 };
 
 export default PeoplesPage;
