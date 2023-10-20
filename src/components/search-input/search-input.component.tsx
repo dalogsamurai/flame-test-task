74ed9a2f3c5bb476fa7607f0a37709706f375f13
dalogsamurai <thelogsamurai@gmail.com>
2023-10-20 13:22:53 +0300

    feat(SearchInput): onChange add

--- src/components/search-input/search-input.component.tsx
+++ src/components/search-input/search-input.component.tsx
@@ -1,12 +1,21 @@
+import { useEffect, useState } from "react";
 import "./search-input.component.sass";
 
 const SearchInput = () => {
+  const [searchStr, setSearchStr] = useState("");
+
+  useEffect(() => {
+    console.log(searchStr);
+
+    // TODO add req
+  }, [searchStr]);
+
   return (
     <div className="">
       <input
         type="text"
         placeholder="Search"
-        onChange={(e) => console.log(e.target.value)}
+        onChange={(e) => setSearchStr(e.target.value)}
       />
     </div>
   );
