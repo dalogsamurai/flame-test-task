a44b37938a2606586fc4bcb8343e77fc4fbe31c3
dalogsamurai <thelogsamurai@gmail.com>
2023-10-20 13:02:55 +0300

    feat: PeopleItem add

---
+++ src/components/people-item/people-item.component.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { IPeople } from "../../types/IPeople";
+import "./people-item.component.sass";
+
+interface Props {
+  item: IPeople;
+}
+
+const PeopleItem = ({ item }: Props) => {
+  // TODO
+  return <div className="">{item.name}</div>;
+};
+
+export default PeopleItem;
