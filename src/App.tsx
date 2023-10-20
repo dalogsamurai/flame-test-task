0a326ad8bd6acce5039b3e7adbe7cbdbed21ee75
dalogsamurai <thelogsamurai@gmail.com>
2023-10-20 11:48:42 +0300

    feat: router-dom add

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Navigate, Route, Router, Routes } from "react-router-dom";
+import MainPage from "./pages/main/main.page";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Router location={''} navigator={}> */}
+      {/* <Layout> */}
+      {/* <Routes> */}
+      {/* <Route path="/" element={<PostsPage />} /> */}
+      {/* <Route path="/people" element={<SettingsPage />}>
+            <Route path=":id" element={<SettingsWallet />} />
+            </Route> */}
+      {/* <Route path="/favorites" element={<MessagesPage />} /> */}
+      {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+      {/* <Pages /> */}
+      {/* </Routes> */}
+      {/* </Layout> */}
+      {/* </Router> */}
+      <Routes>
+        <Route path="/" element={<MainPage />}>
+          {/* <Route index element={<Home />} /> */}
+          {/* <Route path="about" element={<About />} /> */}
+          {/* <Route path="dashboard" element={<Dashboard />} /> */}
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<MainPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
