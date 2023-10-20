ad32c6c0a35d660b3f7355b5ecebb6f7a86aeda6
dalogsamurai <thelogsamurai@gmail.com>
2023-10-20 17:47:01 +0300

    feat: layout add

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/main/main.page";
 import {
-	FAVORITES_PAGE_PATH,
-	MAIN_PAGE_PATH,
-	PEOPLES_PAGE_PATH,
+  FAVORITES_PAGE_PATH,
+  MAIN_PAGE_PATH,
+  PEOPLES_PAGE_PATH,
 } from "./routes/paths";
 import FavoritesPage from "./pages/favorites/favorites.page";
 import Menu from "./components/menu/menu.component";
 import PeoplesPage from "./pages/peoples/peoples.page";
+import "./App.sass";
 
 function App() {
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/" element={<Menu />}>
-					<Route path={MAIN_PAGE_PATH} element={<MainPage />} />
-					<Route path={PEOPLES_PAGE_PATH} element={<PeoplesPage />} />
-					<Route path={FAVORITES_PAGE_PATH} element={<FavoritesPage />} />
-					<Route path="*" element={<MainPage />} />
-				</Route>
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Menu />}>
+          <Route path={MAIN_PAGE_PATH} element={<MainPage />} />
+          <Route path={PEOPLES_PAGE_PATH} element={<PeoplesPage />} />
+          <Route path={FAVORITES_PAGE_PATH} element={<FavoritesPage />} />
+          <Route path="*" element={<MainPage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/components/people-item/people-item.component.tsx
+++ src/components/people-item/people-item.component.tsx
@@ -1,21 +1,25 @@
+import iDelete from "../../assets/delete.svg";
+import iFavorite from "../../assets/favorite.svg";
 import "./people-item.component.sass";
 
 type Props = {
-	height: string;
-	mass: string;
-	name: string;
-	hair_color: string;
+  height: string;
+  mass: string;
+  name: string;
+  hair_color: string;
 };
 
 const PeopleItem = ({ hair_color, height, mass, name }: Props) => {
-	return (
-		<tr>
-			<td>{name}</td>
-			<td>{height}</td>
-			<td>{mass}</td>
-			<td>{hair_color}</td>
-		</tr>
-	);
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{height}</td>
+      <td>{mass}</td>
+      <td>{hair_color}</td>
+      <img src={iFavorite} className="people-item__button" alt="" />
+      {/* <img src={iDelete} className="people-item__button" alt="" /> */}
+    </tr>
+  );
 };
 
 export default PeopleItem;
