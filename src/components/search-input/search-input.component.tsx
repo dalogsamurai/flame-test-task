4a94cba6ddb69bf825ff02c5c6b9257e226b4ed0
dalogsamurai <thelogsamurai@gmail.com>
2023-10-20 17:46:16 +0300

    feat: SearchModal add, req fixes

--- src/components/search-input/search-input.component.tsx
+++ src/components/search-input/search-input.component.tsx
@@ -1,24 +1,48 @@
 import { useEffect, useState } from "react";
+import { IPeople } from "../../types/IPeople";
+import iSearch from "../../assets/search.svg";
+import SearchModal from "../search-modal/search-modal.component";
 import "./search-input.component.sass";
 
 const SearchInput = () => {
-	const [searchStr, setSearchStr] = useState("");
+  const [searchStr, setSearchStr] = useState("");
+  const [searchRes, setSearchRes] = useState<IPeople[]>([]);
+  const [isModalOpen, setModalOpen] = useState(false);
 
-	useEffect(() => {
-		console.log(searchStr);
+  const search = async (str: string) => {
+    const res = await fetch(`https://swapi.dev/api/people/?search=${str}`);
 
-		// TODO add req
-	}, [searchStr]);
+    if (res.ok) {
+      const resData = JSON.parse(await res.text()).results;
+      setSearchRes(resData);
+      setModalOpen(true);
+    }
+  };
 
-	return (
-		<div className="search-input">
-			<input
-				type="text"
-				placeholder="Search"
-				onChange={(e) => setSearchStr(e.target.value)}
-			/>
-		</div>
-	);
+  useEffect(() => {
+    if (searchStr.length > 1) {
+      search(searchStr);
+      setModalOpen(true);
+    } else {
+      setModalOpen(false);
+    }
+  }, [searchStr]);
+
+  return (
+    <div className="search-input">
+      <img src={iSearch} className="search-input__img" alt="" />
+
+      <input
+        type="text"
+        placeholder="Search"
+        className="search-input__input"
+        // @ts-ignore
+        onInput={(e) => setSearchStr(e.target.value)}
+      />
+
+      {searchRes && isModalOpen && <SearchModal items={searchRes} />}
+    </div>
+  );
 };
 
 export default SearchInput;
