0ca1d93d546b32ae04cef0f7a9be186bf2f5c13f
dalogsamurai <thelogsamurai@gmail.com>
2023-10-21 18:09:37 +0300

    fix(Loader): layout fix

--- src/components/loader/loader.component.tsx
+++ src/components/loader/loader.component.tsx
@@ -2,7 +2,11 @@ import iLoading from "../../assets/loading.svg";
 import "./loader.component.sass";
 
 const Loader = () => {
-	return <img src={iLoading} className="loader" alt="" />;
+	return (
+		<div className="loader">
+			<img src={iLoading} className="loader__img" alt="" />
+		</div>
+	);
 };
 
 export default Loader;
