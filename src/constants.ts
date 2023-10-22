5a6fd7e6b5ba4ba104764b88aabccc49b23ccae9
dalogsamurai <thelogsamurai@gmail.com>
2023-10-22 23:54:45 +0300

    feat: delete_people add, codestyle fix

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const ADD_PEOPLE = "ADD_PEOPLE";
+export const DELETE_PEOPLE = "DELETE_PEOPLE";
--- src/index.tsx
+++ src/index.tsx
@@ -5,44 +5,48 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { ADD_PEOPLE, DELETE_PEOPLE } from "./constants";
 import "./index.css";
 
 const defaultState = {
-	peoples: [],
+  peoples: [],
 };
 
 const reducer = (state = defaultState, action: any) => {
-	switch (action.type) {
-		case "ADD_PEOPLE":
-			// @ts-ignore
-			return {
-				...state,
-				peoples: [...state.peoples, action.payload],
-				// peoples: state.peoples.push(action.payload)
-			};
-		case "DELETE_PEOPLE":
-			// @ts-ignore
-			return { ...state, peoples: state.peoples.pop(action.payload) };
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case ADD_PEOPLE:
+      return {
+        ...state,
+        peoples: [...state.peoples, action.payload],
+      };
+    case DELETE_PEOPLE:
+      return {
+        ...state,
+        peoples: state.peoples.filter(
+          // @ts-ignore
+          (people) => people.name !== action.payload.name
+        ),
+      };
+    default:
+      return state;
+  }
 };
 
 // @ts-ignore
 const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement,
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</Provider>
-	</React.StrictMode>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
